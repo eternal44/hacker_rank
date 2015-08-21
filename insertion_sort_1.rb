749be695dbe9082f0648967087f314687cb7cd96
James Youn <jamesyoun710@gmail.com>
2015-08-21 13:21:46 -0700

    Add insertion_sort.rb
    Parameters for the exercise were vague - moving on

--- insertion_sort_1.rb
+++ insertion_sort_1.rb
@@ -1,9 +1,50 @@
 # Shift last item once to lef
 
 array = ("2 4 6 8 3").split.map{|i| i.to_i}
-number_to_sort = array.index(array.last)
+inspected_index = array.index(array.last)
+comparable = array.last
 
-puts array[4 - 0] = array[4 - 1]
 
-puts array.inspect
+array.each do |i|
+  line = ""
+  array[inspected_index] = array[inspected_index - 1]
 
+  if array[inspected_index] < comparable
+    array[1] = comparable
+    array.collect { |x| x.to_s }
+    array.each { |y| line += "#{y} "}
+    puts line
+    break
+  end
+
+  inspected_index -= 1
+  array.collect { |x| x.to_s }
+  array.each { |y| line += "#{y} "}
+  puts line
+end
+
+
+# # Shift last item once to lef
+# def  insertionSort( ar)
+#   array = ar
+#   array = ("2 4 6 8 3").split.map{|i| i.to_i}
+#   inspected_index = array.index(array.last)
+#   comparable = array.last
+
+#   array.each do |i|
+#     array[inspected_index] = array[inspected_index - 1]
+
+#     if array[inspected_index] < comparable
+#       array[1] = comparable
+#       puts array.join(" ").inspect
+#       break
+#     end
+#     inspected_index -= 1
+#     puts array.join(" ").inspect
+#   end
+# end
+
+# count = gets.to_i
+# ar = gets.strip.split.map {|i| i.to_i}
+
+# insertionSort( ar )
